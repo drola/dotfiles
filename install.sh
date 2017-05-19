40133f363e43ff861145e017dcbee8b3f4d84fd1
Matjaž Drolc <mdrolc@gmail.com>
2017-05-19 19:00:34 +0200

    Fixed install script

--- install.sh
+++ install.sh
@@ -6,12 +6,14 @@ GRAY="\033[1;30m"
 
 echo -ne "\n${GREEN}Initializing...\n\n"
 echo -e "Creating backup & creating symlinks to new dotfiles..."
+SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 
-cd ~/.dotfiles/files
-for file in *; do
-  echo "~/.$file"
-  if [ -s ~/.$file ]; then mv ~/.$file ~/.$file.bkp; fi
-  ln -s ~/.dotfiles/files/$file ~/.$file
+cd $SCRIPT_PATH; #To dir with this script
+cd ./files
+for file in $(find . -type f); do
+  echo "~/$file"
+  if [ -s ~/$file ]; then mv ~/$file ~/$file.bkp; fi
+  ln -s ~/$SCRIPT_PATH/files/$file ~/$file
 done
 
 echo -e "${GRAY}"
